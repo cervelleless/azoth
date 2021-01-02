654bd55d5a1df717a1d97023d86b8cd767ee1032
cervelleless <cervelleless@gmail.com>
2021-01-02 08:24:09 +0800

    replace js2-mode with js-mode

--- elisp/salt-web.el
+++ elisp/salt-web.el
@@ -20,13 +20,11 @@
 ;;; Code:
 
 ;; js2-mode
-(use-package js2-mode
-  :straight t
-  :mode ("\\.js\\'" . js2-mode)
+(use-package js-mode
   :init
   (setq js-indent-level 2)
   :hook
-  (js2-mode . subword-mode)
+  (js-mode . subword-mode)
   :defer t)
 
 (use-package simple-httpd
