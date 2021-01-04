adee02ef93e3b31d696eea3ac3cb6fc06a12c59c
cervelleless <cervelleless@gmail.com>
2021-01-05 05:07:51 +0800

    dump/load environment variables to/from file

--- elisp/salt-env.el
+++ elisp/salt-env.el
@@ -1,18 +1,5 @@
 ;;; salt-env.el --- environment variables and settings -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2020 Cervelleless
-;;
-;; Author: Cervelleless <http://github.com/cervelleless>
-;; Maintainer: Cervelleless <cervelleless@gmail.com>
-;; Created: December 02, 2020
-;; Modified: December 02, 2020
-;; Version: 0.0.1
-;; Keywords:
-;; Homepage:
-;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
-;;
-;; This file is not part of GNU Emacs.
-;;
 ;;; Commentary:
 ;;
 ;;  environment variables and settings.
@@ -20,6 +7,7 @@
 ;;; Code:
 
 ;;; some settings
+;; no tabs
 (setq indent-tabs-mode nil)
 
 ;; y-or-n
@@ -29,8 +17,8 @@
 (setq vc-handled-backends nil)
 
 ;; custom variables
-(setq custom-file (concat user-emacs-directory "private/custom.el"))
-(setq auto-save-list-file-prefix (concat user-emacs-directory "private/autosave/"))
+(setq custom-file (expand-file-name "custom.el" salt-dir-private))
+
 
 ;; warn when opening files bigger than 100MB
 (setq large-file-warning-threshold (* 100 1000 1000))
@@ -41,23 +29,42 @@
 ;; default directory
 (setq default-directory (concat (getenv "HOME") "/"))
 
-;; GNUS cann't send email error
-(add-to-list 'exec-path "/usr/local/bin/")
-;; shell-command-to-string error
-(setenv "PATH"
-        (concat "/usr/local/bin/:"
-                "/usr/local/opt/coreutils/libexec/gnubin/:"
-                (getenv "PATH")))
-;; eshll-mode command cann't found
-(eval-after-load 'esh-util
-  '(progn
-     (setq eshell-path-env (concat "/usr/local/bin/:" "/usr/local/opt/coreutils/libexec/gnubin/:" eshell-path-env))))
-
 ;; inhibit startup screen
 (setq inhibit-startup-screen t)
 
 ;; Set up the visible bell
 (setq visible-bell t)
+
+
+;;; dump/load data to file
+;; dump to file
+(require 'cl-lib)
+(defun salt/dump-to-file (filename data)
+  (with-temp-file filename
+    (prin1 data (current-buffer))))
+
+;; load from file
+(defun salt/load-from-file (filename)
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (cl-assert (eq (point) (point-min)))
+    (read (current-buffer))))
+
+(let ((env-file (expand-file-name "env" salt-dir-local))
+      (shell-cmd "source ~/.zshrc; env"))
+  (if (file-exists-p env-file)
+      (setq process-environment (salt/load-from-file env-file))
+    (progn (message "Regenerate env file.")
+	   (setq process-environment (split-string (shell-command-to-string shell-cmd) "\n"))
+	   (salt/dump-to-file env-file process-environment))))
+
+;;; copy from Bozhidar Batsov. HACK!!
+(let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
 
 ;;; envrc
 (use-package envrc
