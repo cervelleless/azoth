fbb9a883f18a1d1fd955ee23b48f414f11ff6f28
cervelleless <cervelleless@gmail.com>
2021-01-04 22:29:54 +0800

    move recentf to salt-editor

--- elisp/salt-files.el
+++ elisp/salt-files.el
@@ -1,24 +1,10 @@
 ;;; salt-files.el --- file, directory and something else -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2020 cervelle
-;;
-;; Author: Cervelleless <http://github.io/cervelleless>
-;; Maintainer: Cervelle <cervelleless@gmail.com>
-;; Created: November 17, 2020
-;; Modified: November 17, 2020
-;; Version: 0.0.1
-;; Keywords:
-;; Homepage:
-;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
-;;
-;; This file is not part of GNU Emacs.
-;;
 ;;; Commentary:
 ;;
 ;;  file, directory and something else.
 ;;
 ;;; Code:
-
 
 
 ;; ranger
@@ -34,15 +20,6 @@
              ranger-ignored-extensions '("mkv" "flv" "iso" "mp4")))
 
 
-;; recentf
-(use-package recentf
-  :straight t
-  :defer t
-  :commands recentf-mode
-  :hook (after-init . recentf-mode)
-  :config
-  (setq recentf-max-saved-items 200
-        recentf-max-menu-items 15))
  
 (provide 'salt-files)
 ;;; salt-files ends here
