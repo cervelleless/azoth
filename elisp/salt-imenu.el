ef9c0a2fcbd64ffa667419ade78bf5a70c2c48eb
cervelleless <cervelleless@gmail.com>
2021-01-06 06:57:12 +0800

    imenu

---
+++ elisp/salt-imenu.el
@@ -0,0 +1,29 @@
+;;; salt-imenu.el --- imenu -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Cervelle
+;;
+;; Author: Cervelleless <http://github.io/cervelleless>
+;; Maintainer: Cervelle <cervelleless@gmail.com>
+;; Created: November 17, 2020
+;; Modified: November 17, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage:
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  The settings of imenu.
+;;
+;;; Code:
+
+(use-package imenu)
+
+(use-package flimenu
+  :straight t)
+
+
+(provide 'salt-imenu)
+;;; salt-imenu.el ends here
