72a00dd2edefa3a4caa4e9eb577de5596327df34
cervelleless <cervelleless@gmail.com>
2021-01-05 11:00:50 +0800

    lang: ruby

---
+++ elisp/salt-ruby.el
@@ -0,0 +1,38 @@
+;;; salt-ruby.el --- ruby -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;
+;;  ruby
+;;
+;;; Code:
+
+;; ruby
+(use-package enh-ruby-mode
+  :straight t
+  :defer t)
+
+(use-package inf-ruby
+  :straight t
+  :defer t)
+
+(use-package robe
+  :straight t
+  :defer t)
+
+(use-package rubocop
+  :straight t
+  :defer t)
+
+(use-package rake
+  :straight t
+  :defer t)
+
+(use-package bundler
+  :straight t
+  :defer t)
+
+(provide 'salt-ruby)
+;;; salt-ruby.el ends here
+
+
+  
