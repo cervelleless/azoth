ef0d53a9ea63a3290386b2ee5252941072947385
cervelleless <cervelleless@gmail.com>
2021-01-26 10:05:53 +0800

    remove package: consult-selectrum

--- elisp/salt-completion.el
+++ elisp/salt-completion.el
@@ -24,6 +24,31 @@
   :straight t
   :hook (after-init . selectrum-mode))
 
+;; (use-package icomplete-vertical
+;;   :straight t
+;;   :init
+;;   (setq completion-styles '(partial-completion substring)
+;;	completion-category-overrides '((file (styles basic substring)))
+;;	read-file-name-completion-ignore-case t
+;;	read-buffer-completion-ignore-case t
+;;	completion-ignore-case t)
+;;   :hook
+;;   (after-init . icomplete-mode)
+;;   (icomplete-mode . icomplete-vertical-mode)
+;;   :bind (:map icomplete-minibuffer-map
+;;               ("<down>" . icomplete-forward-completions)
+;;               ("C-n" . icomplete-forward-completions)
+;;               ("<up>" . icomplete-backward-completions)
+;;               ("C-p" . icomplete-backward-completions)
+;;               ("C-v" . icomplete-vertical-toggle)))
+
+(use-package orderless
+  :straight t
+  :init
+  (setq selectrum-refine-candidates-function #'orderless-filter
+	selectrum-highlight-candidates-function #'orderless-highlight-matches))
+
+
 ;;; consult
 (use-package consult
   :straight t
@@ -45,21 +70,18 @@
   (setq prescient-save-file (expand-file-name "prescient-save.el" salt-dir-cache))
   :hook (selectrum-mode . prescient-persist-mode)
   (selectrum-mode . selectrum-prescient-mode))
-        
-;;; consult-selectrum
-(use-package consult-selectrum
-  :straight t)
+
 
 ;;; embark
-(use-package embark
-  :straight t)
+  (use-package embark
+    :straight t)
 
 ;;; Enable richer annotations using the Marginalia package
 (use-package marginalia
   :straight t
   ;; When using the Embark package, you can bind `marginalia-cycle' as an Embark action!
   :bind (:map embark-general-map
-              ("A" . marginalia-cycle))
+	      ("A" . marginalia-cycle))
   ;; The :init configuration is always executed (Not lazy!)
   :init
   ;; Must be in the :init section of use-package such that the mode gets
@@ -68,7 +90,7 @@
   ;; When using Selectrum and `marginalia-cycle' as an Embark action,
   ;; ensure that Selectrum is refreshed when cycling annotations.
   (advice-add #'marginalia-cycle :after
-              (lambda () (when (bound-and-true-p selectrum-mode) (selectrum-exhibit))))
+	      (lambda () (when (bound-and-true-p selectrum-mode) (selectrum-exhibit))))
   ;; Prefer richer, more heavy, annotations over the lighter default variant.
   ;; E.g. M-x will show the documentation string additional to the keybinding.
   ;; By default only the keybinding is shown as annotation.
